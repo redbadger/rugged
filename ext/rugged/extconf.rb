87750b4aac38fe9d7c6a0fd2310f5b1a091bc89b
Viktor Charypar <charypar@gmail.com>
2014-02-28 13:00:05 +0000

    Always use vendored libs for now

--- ext/rugged/extconf.rb
+++ ext/rugged/extconf.rb
@@ -37,44 +37,43 @@ if MAKE_PROGRAM.nil?
   abort "ERROR: GNU make is required to build Rugged"
 end
 
-if preserving_globals { dir_config('git2') }.any?
-  dir_config('git2')
+CWD = File.expand_path(File.dirname(__FILE__))
+ROOT_DIR = File.join(CWD, '..', '..')
+LIBGIT2_DIR = File.join(CWD, '..', '..', 'vendor', 'libgit2')
 
-  unless have_library 'git2' and have_header 'git2.h'
-    abort "ERROR: Can not find libgit2 in the specified path"
+# always use vendored libgit2
+
+Dir.chdir(ROOT_DIR) do
+  sys('git submodule update --init')
+end
+
+if find_executable('cmake')
+  Dir.chdir(LIBGIT2_DIR) do
+    Dir.mkdir("build") if !Dir.exists?("build")
+
+    Dir.chdir("build") do
+      sys("cmake .. -DBUILD_CLAR=OFF -DTHREADSAFE=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS=-fPIC ")
+      # Grab the flags from an authoritative source
+      pcfile = File.join(LIBGIT2_DIR, "build", "libgit2.pc")
+      $LDFLAGS << " " + `pkg-config --libs --static #{pcfile}`.strip
+      sys(MAKE_PROGRAM)
+    end
   end
+
+  dir_config('git2', "#{LIBGIT2_DIR}/include", "#{LIBGIT2_DIR}/build")
 else
-  CWD = File.expand_path(File.dirname(__FILE__))
-  LIBGIT2_DIR = File.join(CWD, '..', '..', 'vendor', 'libgit2')
+  warn "WARN: CMake was not found!"
+  warn "WARN: Rugged will be built without ssl or ssh support!"
 
-  if find_executable('cmake')
-    Dir.chdir(LIBGIT2_DIR) do
-      Dir.mkdir("build") if !Dir.exists?("build")
-
-      Dir.chdir("build") do
-        sys("cmake .. -DBUILD_CLAR=OFF -DTHREADSAFE=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS=-fPIC ")
-        # Grab the flags from an authoritative source
-        pcfile = File.join(LIBGIT2_DIR, "build", "libgit2.pc")
-        $LDFLAGS << " " + `pkg-config --libs --static #{pcfile}`.strip
-        sys(MAKE_PROGRAM)
-      end
-    end
-
-    dir_config('git2', "#{LIBGIT2_DIR}/include", "#{LIBGIT2_DIR}/build")
-  else
-    warn "WARN: CMake was not found!"
-    warn "WARN: Rugged will be built without ssl or ssh support!"
-
-    Dir.chdir(LIBGIT2_DIR) do
-      sys("#{MAKE_PROGRAM} -f Makefile.embed")
-    end
-
-    dir_config('git2', "#{LIBGIT2_DIR}/include", LIBGIT2_DIR)
+  Dir.chdir(LIBGIT2_DIR) do
+    sys("#{MAKE_PROGRAM} -f Makefile.embed")
   end
 
-  unless have_library 'git2' and have_header 'git2.h'
-    abort "ERROR: Failed to build libgit2"
-  end
+  dir_config('git2', "#{LIBGIT2_DIR}/include", LIBGIT2_DIR)
+end
+
+unless have_library 'git2' and have_header 'git2.h'
+  abort "ERROR: Failed to build libgit2"
 end
 
 # Compile backends
