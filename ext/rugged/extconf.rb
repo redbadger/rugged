170fce01a5ad2eba9cd8ad00b40ad42c01bbd6f7
Viktor Charypar <charypar@gmail.com>
2014-03-03 14:22:27 +0000

    Vendor reids

--- ext/rugged/extconf.rb
+++ ext/rugged/extconf.rb
@@ -76,6 +76,19 @@ unless have_library 'git2' and have_header 'git2.h'
   abort "ERROR: Failed to build libgit2"
 end
 
+
+hiredis_dir = File.expand_path(File.join(File.dirname(__FILE__), %w{.. .. vendor hiredis}))
+unless File.directory?(hiredis_dir)
+  STDERR.puts "vendor/hiredis missing, please checkout its submodule..."
+  exit 1
+end
+
+# Make sure hiredis is built...
+system("cd #{hiredis_dir} && make static")
+
+# Statically link to hiredis (mkmf can't do this for us)
+
+dir_config('hiredis', hiredis_dir, hiredis_dir)
 have_library('hiredis') or raise
 
 # Compile backends
